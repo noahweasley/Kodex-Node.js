6feebd922dc3deb8fadffaf37b5a98aa56643edf
noahweasley <iebenmelu@gmail.com>
2025-02-05 11:41:24 +0100

    Update assignment

--- streams/file-streams.js
+++ streams/file-streams.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
+const fsp = require("fs/promises");
 
-const stream = fs.createReadStream("./streams/file-streams.js");
-const stream2 = fs.createWriteStream("./new-file.js");
+(async function main() {
+  const stream = fs.createReadStream("./streams/file-streams.js");
+  const stream2 = fs.createWriteStream("./output/new-file.js");
+  await fsp.mkdir("./output");
 
-stream.on("data", (chunk) => {
-  stream2.write(chunk.toString());
-});
+  stream.on("data", (chunk) => {
+    stream2.write(chunk.toString());
+    console.log("Successful");
+  });
+})();
