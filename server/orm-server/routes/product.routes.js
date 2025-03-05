4303b078d9e16127c328c8fa188aa74c6c284273
noahweasley <iebenmelu@gmail.com>
2025-03-05 09:39:06 +0100

    Add Joi validation

--- server/orm-server/routes/product.routes.js
+++ server/orm-server/routes/product.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const productController = require("../controllers/products.controller");
+const productSchema = require("../schema/product.schema");
 
 const router = express.Router();
 
 router.get("/", productController.getProducts);
 router.get("/:id", productController.getProductById);
-router.post("/", productController.createProduct);
+router.post("/", validator(productSchema), productController.createProduct);
 router.put("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
 
